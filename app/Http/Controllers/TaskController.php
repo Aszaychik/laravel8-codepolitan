826ea51adc937663456b91c4219a2e1bb1a22a39
AsZaychik <aszaychik@gmail.com>
2022-11-26 06:48:01 +0700

    Menggunakan Model Untuk Operasi CRUD

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,25 +12,24 @@ class TaskController extends Controller
     public function index(Request $request){
         // ddd(request()->all());
         if($request->search){
-            $query = DB::table('tasks')
-                ->where('task', 'LIKE', "%$request->search%")
+            $query = Task::where('task', 'LIKE', "%$request->search%")
                 ->get();
             return $query;
         };
 
-        $query = DB::table('tasks')->get();
+        $query = Task::all();
         return $query;
     }
 
     public function show($id)
     {
-        $query = DB::table('tasks')->where('id', $id)->first();
-        ddd($query);
+        $query = Task::find($id);
+        return $query;
     }
 
     public function store(Request $request)
     {
-        DB::table('tasks')->insert([
+        Task::create([
             'task' => $request->task,
             'user' => $request->user,
         ]);
@@ -39,7 +39,7 @@ class TaskController extends Controller
 
     public function update(Request $request, $id)
     {
-        DB::table('tasks')->where('id', $id)->update([
+        Task::find($id)->update([
             'task' => $request->task,
             'user' => $request->user
         ]);
@@ -48,7 +48,7 @@ class TaskController extends Controller
 
     public function destroy($id)
     {
-        DB::table('tasks')->where('id', $id)->delete();
+        Task::find($id)->delete();
 
         return "Delete Data Success";
     }
