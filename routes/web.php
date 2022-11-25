915032eb40399bb1ff22caa45ab460d343fd8243
AsZaychik <aszaychik@gmail.com>
2022-11-26 04:48:07 +0700

    Menggunakan Method DELETE

--- routes/web.php
+++ routes/web.php
@@ -74,3 +74,8 @@ Route::put('/tasks/{key}', function($key) use($taskList){
     $taskList[$key] = request()->task;
     return $taskList;
 });
+
+Route::delete('/tasks/{key}', function($key) use($taskList){
+    unset($taskList[$key]);
+    return $taskList;
+});
