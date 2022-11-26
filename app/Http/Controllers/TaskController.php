c2c3203123328dfd35dc16db5a90eb7a7fe09de8
AsZaychik <aszaychik@gmail.com>
2022-11-26 08:43:04 +0700

    Memanfaatkan Form Request Untuk Validasi Input

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -35,13 +36,8 @@ class TaskController extends Controller
         return view('task.create');
     }
 
-    public function store(Request $request)
+    public function store(TaskRequest $request)
     {
-        $request->validate([
-            'task' => ['required'],
-            'user' => ['required']
-        ]);
-
         Task::create([
             'task' => $request->task,
             'user' => $request->user,
@@ -56,7 +52,7 @@ class TaskController extends Controller
         return view('task.edit', compact('data'));
     }
 
-    public function update(Request $request, $id)
+    public function update(TaskRequest $request, $id)
     {
         Task::find($id)->update([
             'task' => $request->task,
