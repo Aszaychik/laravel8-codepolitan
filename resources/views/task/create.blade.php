4010fef7dc5fe4c0fbac3be55e4eeffee06a8c5a
AsZaychik <aszaychik@gmail.com>
2022-11-26 08:48:04 +0700

    Menampilkan Pesan Error Di Masing-Masing Field

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -2,15 +2,6 @@
 
 @section('main')
 <div class="mt-5 mx-auto" style="width: 380px">
-    @if ($errors->any())
-        <div class="alert alert-danger text-uppercase">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <div class="card">
         <div class="card-body">
             <form action="{{ url('/tasks')}}" method="POST">
@@ -18,10 +9,16 @@
                 <div class="mb-3">
                     <label for="user" class="form-label">User</label>
                     <input name="user" type="text" class="form-control">
+                    @error('user')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="task" class="form-label">Task</label>
                     <textarea name="task" class="form-control" id="" rows="3"></textarea>
+                    @error('task')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -2,15 +2,6 @@
 
 @section('main')
 <div class="mt-5 mx-auto" style="width: 380px">
-    @if ($errors->any())
-        <div class="alert alert-danger text-uppercase">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <div class="card">
         <div class="card-body">
             <form action="{{ url("/tasks/$data->id") }}" method="POST">
@@ -19,10 +10,16 @@
                 <div class="mb-3">
                     <label for="" class="form-label">User</label>
                     <input name="user" type="text" class="form-control" value="{{ $data->user}}">
+                    @error('user')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="" class="form-label">Task</label>
                     <textarea name="task" class="form-control" id="" rows="3" >{{ $data->task}}</textarea>
+                    @error('task')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Update</button>
             </form>
